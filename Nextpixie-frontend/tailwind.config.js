be832c6af319957fdbcd130bb74649dc08a3de6a
Henzyd <henzydee@gmail.com>
2023-04-02 21:42:06 +0100

    Added custom shadow to the navbar

--- Nextpixie-frontend/tailwind.config.js
+++ Nextpixie-frontend/tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      NavBarboxShadow: "0px 2px 12px 0px #00203F29",
+    },
   },
   plugins: [],
 };
